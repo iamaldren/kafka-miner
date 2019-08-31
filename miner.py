4cb55331c8148e731f1f1b3ea5691a538fd675a5
iamaldrew <aldrenbobiles@gmail.com>
2019-08-31 10:51:38 +0800

    Updated consumer to get data from the last committed data in the topic

--- miner.py
+++ miner.py
@@ -36,7 +36,7 @@ class Consumer(multiprocessing.Process):
 
     def run(self):
         consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
-                                 auto_offset_reset='earliest',
+                                 auto_offset_reset='latest',
                                  consumer_timeout_ms=1000,
                                  value_deserializer=lambda m: json.loads(m.decode('utf-8')))
         consumer.subscribe(['test'])
